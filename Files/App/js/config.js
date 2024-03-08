0613865bd789cc0093edbfaa612d6967ad22cd47
sunthorn wisaidee <sunthorn.w@cdg.co.th>
2024-03-08 15:23:58 +0700

    fix bug

--- Files/App/js/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Config file
- *
- * a global variable to store the room layer layer once it has been added to the map
- * global as will need to be referened by the room layer remove functionality
- *
- */
-const baseUrl = "http://localhost:4480/api/geojson24/geojson"; //document.location.origin
